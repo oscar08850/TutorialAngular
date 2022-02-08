e2ac5f3c268a7be45e03556d417f0d6db0ab016d
oscar08850 <oscar08850@gmail.com>
2022-02-08 19:31:44 +0100

    final leccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { PersonaComponent } from './persona/persona.component';
+import { ProfesorComponent } from './profesor/profesor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculadoraComponent,
-    PersonaComponent
+    PersonaComponent,
+    ProfesorComponent
   ],
   imports: [
     BrowserModule,
