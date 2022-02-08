c8f38f927112337875dd9fb24658ce3607fc0610
oscar08850 <oscar08850@gmail.com>
2022-02-08 16:32:11 +0100

    Final leccion 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoTutorial';
+  numero: number;
+  resultado: number;
+  Duplicar () {
+    console.log (`Voy a duplicar ${this.numero} y el resultado es:`)
+    this.resultado = this.numero * 2;
+    console.log (this.resultado) 
+
+  }
 }
